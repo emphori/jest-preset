dea63e1076952c686b5ea85c0576b3c65974f112
Iain Reid <iainreid382@gmail.com>
2019-08-13 00:09:27 +0100

    Correct module path mapping

--- jest-preset.js
+++ jest-preset.js
@@ -1,46 +1,82 @@
 'use strict';
 
 const fs = require('fs');
+const glob = require('glob');
+const path = require('path');
 
-const config = {
+const { name, main } = readPackageJSON();
+
+let moduleDirectories = [];
+let moduleNameMapper = {};
+
+if (fs.existsSync('lerna.json')) {
+  moduleDirectories = getLernaRepoPackagesRoot();
+  moduleNameMapper = getLernaRepoPackagesMap();
+}
+
+moduleDirectories = [
+  ...moduleDirectories,
+  'node_modules'
+];
+
+moduleNameMapper = {
+  ...moduleNameMapper,
+  [`^${name}$`]: path.join('<rootDir>', main || '')
+}
+
+module.exports = {
   verbose: true,
   collectCoverage: true,
   collectCoverageFrom: [
     'lib/**/*.{js,ts}',
     'src/main/**/*.{js,ts}'
+  ],
+  projects: [
+    {
+      preset: 'ts-jest',
+      testRegex: '\\w+\\.spec\\.ts$',
+      moduleDirectories,
+      moduleNameMapper,
+      globals: {
+        'ts-jest': {
+          'diagnostics': {
+            'ignoreCodes': [
+              'TS151001'
+            ]
+          }
+        }
+      }
+    },
+    {
+      testRegex: '\\w+\\.spec\\.js$',
+      moduleDirectories,
+      moduleNameMapper
+    }
   ]
 };
 
-if (fs.existsSync('tsconfig.json')) {
-  console.log('TS project detected');
+function getLernaRepoPackagesRoot() {
+  return readLernaJSON().packages;
+}
 
-  config.preset = 'ts-jest';
-  config.globals = {
-    'ts-jest': {
-      'diagnostics': {
-        'ignoreCodes': [
-          'TS151001'
-        ]
-      }
+function getLernaRepoPackagesMap() {
+  const packagesMap = {};
+
+  for (const packageRoot of getLernaRepoPackagesRoot()) {
+    for (const packageDir of glob.sync(packageRoot)) {
+      const { name, main } = readPackageJSON(packageDir);
+
+      packagesMap[`^${name}$`] = path.join('<rootDir>', packageDir, main || '');
     }
   }
+
+  return packagesMap;
 }
 
-if (fs.existsSync('packages')) {
-  console.log('Monorepo detected');
-
-  config.collectCoverageFrom = [
-    'packages/*/lib/**/*.{js,ts}',
-    'packages/*/src/main/**/*.{js,ts}'
-  ],
-
-  config.moduleNameMapper = {};
-
-  for (const dir of fs.readdirSync('packages')) {
-    const { name } = JSON.parse(fs.readFileSync(`packages/${dir}/package.json`));
-
-    config.moduleNameMapper[`^${name}/([a-z]+)$`] = `<rootDir>/packages/${dir}/$1`;
-  };
+function readLernaJSON(root = '.') {
+  return JSON.parse(fs.readFileSync(`${root}/lerna.json`));
 }
 
-module.exports = config;
+function readPackageJSON(root = '.') {
+  return JSON.parse(fs.readFileSync(`${root}/package.json`));
+}
